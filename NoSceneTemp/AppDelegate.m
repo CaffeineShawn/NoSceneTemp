fb069260d6f64bf710613735c87a201eee1d9c23
CaffeineShawn <52215406+CaffeineShawn@users.noreply.github.com>
2022-06-05 23:42:12 +0800

    init

--- NoSceneTemp/AppDelegate.m
+++ NoSceneTemp/AppDelegate.m
@@ -8,7 +8,6 @@
 #import "AppDelegate.h"
 
 @interface AppDelegate ()
-
 @end
 
 @implementation AppDelegate
@@ -16,25 +15,21 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    UITabBarController *tabVC = UITabBarController.new;
+    _window = UIWindow.new;
+    _window.rootViewController = tabVC;
+    UIViewController *firstVC = UIViewController.new;
+    UINavigationController *secondVC = UINavigationController.new;
+    tabVC.viewControllers = @[firstVC, secondVC];
+    firstVC.tabBarItem.title = @"Videos";
+    firstVC.tabBarItem.image = [UIImage systemImageNamed:@"play.circle.fill"];
+    
+    secondVC.tabBarItem.title = @"User";
+    secondVC.tabBarItem.image = [UIImage systemImageNamed:@"person.circle"];
+    secondVC.navigationBarHidden = YES;
+    [_window makeKeyAndVisible];
+    
     return YES;
 }
-
-
-#pragma mark - UISceneSession lifecycle
-
-
-- (UISceneConfiguration *)application:(UIApplication *)application configurationForConnectingSceneSession:(UISceneSession *)connectingSceneSession options:(UISceneConnectionOptions *)options {
-    // Called when a new scene session is being created.
-    // Use this method to select a configuration to create the new scene with.
-    return [[UISceneConfiguration alloc] initWithName:@"Default Configuration" sessionRole:connectingSceneSession.role];
-}
-
-
-- (void)application:(UIApplication *)application didDiscardSceneSessions:(NSSet<UISceneSession *> *)sceneSessions {
-    // Called when the user discards a scene session.
-    // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-    // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-}
-
 
 @end
